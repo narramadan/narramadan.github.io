47203c107f6c6ca8afb7cabae22ca01fe977933e
Madan Narra <narra.madan@outlook.com>
2021-10-17 19:33:20 +1100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-09d9993072be67a4f94b.js"
+    "url": "webpack-runtime-9b6d8f6d10d12f52685d.js"
   },
   {
     "url": "styles.dfb58686fcdf3594c809.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b72b583b732431f9a360c4ad63ee2016"
+    "revision": "fe824422bfd8540238cba76a3e510a02"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-4320744c1a776ea750bd.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "f12f18751b748481b47297ac590ef312"
+    "revision": "082ee3d435edaa544527303121b61377"
   },
   {
     "url": "polyfill-32954798fd23aedc8c12.js"
